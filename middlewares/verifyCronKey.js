56ac1596b8155b05b4ed9a81715742be748f531e
d12ilham <d12ilham@gmail.com>
2025-07-22 04:45:15 +0530

    fixed verified token

--- middlewares/verifyCronKey.js
+++ middlewares/verifyCronKey.js
@@ -1,10 +1,15 @@
 // middleware/verifyCronKey.js
-const CRON_SECRET_KEY = process.env.CRON_SECRET_KEY || "yoursecret";
 
-export function verifyCronKey(req, res, next) {
+export const verifyCronKey = (req, res, next) => {
   const key = req.query.key;
-  if (key !== CRON_SECRET_KEY) {
+
+  console.log("key", key);
+  if (!process.env.CRON_SECRET_KEY) {
+    console.warn("Warning: CRON_SECRET_KEY not set in environment!");
+  }
+
+  if (key !== process.env.CRON_SECRET_KEY) {
     return res.status(403).json({ message: "Forbidden" });
   }
   next();
-}
+};
